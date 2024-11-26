93c8e68b2ae848fa538e5aa83cd39e0caa173de3
fatmaatef304 <fatimaatef990@gmailcom>
2024-11-26 13:13:43 +0200

    add some styles

--- lib/core/theme/app_text_style.dart
+++ lib/core/theme/app_text_style.dart
@@ -1,19 +1,77 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:greenify_app/core/theme/font_family_helper.dart';
+import 'package:greenify_app/core/theme/app_color.dart';
 import 'package:greenify_app/core/theme/font_weight_helper.dart';
 
 class AppTextStyle {
-  static TextStyle font37WhiteBold = TextStyle(
-    fontSize: 37.sp,
-    color: Colors.white,
-    fontWeight: FontWeightHelper.bold,
-    fontFamily: FontFamilyHelper.montserratFont,
-  );
   static TextStyle font14WhiteMedium = TextStyle(
     fontSize: 14.sp,
     color: Colors.white,
     fontWeight: FontWeightHelper.medium,
-    fontFamily: FontFamilyHelper.montserratFont,
+  );
+
+   static TextStyle font16WhiteMedium = TextStyle(
+    fontSize: 16.sp,
+    color: Colors.white,
+    fontWeight: FontWeightHelper.medium,
+  );
+
+  static TextStyle font20WhiteMedium = TextStyle(
+    fontSize: 20.sp,
+    color: Colors.white,
+    fontWeight: FontWeightHelper.medium,
+  );
+  static TextStyle font37WhiteBold = TextStyle(
+    fontSize: 37.sp,
+    color: Colors.white,
+    fontWeight: FontWeightHelper.bold,
+  );
+  static TextStyle font14BlackMedium = TextStyle(
+    fontSize: 14.sp,
+    color: AppColor.black,
+    fontWeight: FontWeightHelper.medium,
+  );
+  static TextStyle font16BlackSemiBold = TextStyle(
+    fontSize: 16.sp,
+    color: AppColor.black,
+    fontWeight: FontWeightHelper.semiBold,
+  );
+  static TextStyle font20BlackMedium = TextStyle(
+    fontSize: 20.sp,
+    color: AppColor.black,
+    fontWeight: FontWeightHelper.medium,
+  );
+  static TextStyle font14LightGreenSemiBold = TextStyle(
+    fontSize: 14.sp,
+    color: AppColor.darkGreen,
+    fontWeight: FontWeightHelper.semiBold,
+  );
+  static TextStyle font16DarkGreenMedium = TextStyle(
+    fontSize: 14.sp,
+    color: AppColor.darkGreen,
+    fontWeight: FontWeightHelper.medium,
+  );
+
+  static TextStyle font18DarkGreenRegular = TextStyle(
+    fontSize: 18.sp,
+    color: AppColor.darkGreen,
+    fontWeight: FontWeightHelper.regular,
+  );
+
+  static TextStyle font16RedRegular = TextStyle(
+    fontSize: 16.sp,
+    color: AppColor.red,
+    fontWeight: FontWeightHelper.regular,
+  );
+  static TextStyle font12LightGreyMedium = TextStyle(
+    fontSize: 12.sp,
+    color: AppColor.lightGrey,
+    fontWeight: FontWeightHelper.medium,
+  );
+
+  static TextStyle font16LightGreySemiBold = TextStyle(
+    fontSize: 16.sp,
+    color: AppColor.darkGrey,
+    fontWeight: FontWeightHelper.semiBold,
   );
 }
--- lib/core/theme/font_family_helper.dart
+++ lib/core/theme/font_family_helper.dart
@@ -2,5 +2,6 @@ class FontFamilyHelper{
 
   static const String poppinsFont = 'Poppins';
   static const String montserratFont = 'Montserrat';
+  static const String montaguSlabFont = 'Montagu_Slab';
 
 }
