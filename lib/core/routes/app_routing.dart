b5b0b3607732412299632d110a5b73f8ada9509b
fatmaatef304 <fatimaatef990@gmailcom>
2024-12-01 13:28:12 +0200

    add routes

--- lib/core/routes/app_routing.dart
+++ lib/core/routes/app_routing.dart
@@ -14,6 +14,7 @@ class AppRouting {
         return MaterialPageRoute(
           builder: (_) => const OnBoardingScreen(),
         );
+        
 
       default:
         return MaterialPageRoute(
